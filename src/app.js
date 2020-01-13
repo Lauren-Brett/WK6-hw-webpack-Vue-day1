476d0c73c34ee2bdca83142c5a213fe046ebbc6f
lauren brett <laurencareybrett@gmail.com>
2020-01-13 16:25:33 +0000

    start

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('hw is up');
+    // new Vue( {
+    //     el: "#app",
+    //     data: {
+    //         todos: [
+
+    //         ]
+    //     }
+
+
+    // })
+})
